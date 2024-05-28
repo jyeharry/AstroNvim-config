c039b6a466b9c077cdec3bda183dd4e2716af676
Jye Harry <jyeharry21@hotmail.com>
2024-05-29 00:46:15 +1000

    Prevent keymaps being overwritten

--- init.lua
+++ init.lua
@@ -17,3 +17,28 @@ end
 
 require "lazy_setup"
 require "polish"
+
+local function set_keymap(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+local function set_keymaps()
+  set_keymap('n', '<C-l>', ':bnext<CR>', { desc = "Next buffer" })
+  set_keymap('n', '<C-h>', ':bprevious<CR>', { desc = "Previous buffer" })
+  set_keymap('n', "<C-k>", "<C-u>", { desc = "Scroll up half a page" })
+  set_keymap('n', "<C-j>", "<C-d>", { desc = "Scroll down half a page" })
+end
+
+set_keymaps()
+
+-- if needed, create an autocmd that runs on BufEnter
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'LazyVimDone',
+  callback = function()
+    set_keymaps()
+  end,
+})
