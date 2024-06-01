c09e761ae9e820a018480df6b928231b10b7cf35
Jye Harry <jye.harry@nine.com.au>
2024-06-01 19:23:18 +1000

    Remove redundant mappings for neotest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -6,20 +6,27 @@ return {
     opts = {
       mappings = {
         n = {
-          [prefix] = { desc = "󰗇 Tests" },
-          [prefix .. "t"] = { function() require("neotest").run.run() end, desc = "Run test" },
-          [prefix .. "d"] = { function() require("neotest").run.run { strategy = "dap" } end, desc = "Debug test" },
+          [prefix .. "t"] = {
+            function()
+              require("neotest").output_panel.clear()
+              require("neotest").run.run()
+            end,
+            desc = "Run test",
+          },
           [prefix .. "f"] = {
-            function() require("neotest").run.run(vim.fn.expand "%") end,
+            function()
+              require("neotest").output_panel.clear()
+              require("neotest").run.run(vim.fn.expand "%")
+            end,
             desc = "Run all tests in file",
           },
           [prefix .. "p"] = {
-            function() require("neotest").run.run(vim.fn.getcwd()) end,
+            function()
+              require("neotest").output_panel.clear()
+              require("neotest").run.run(vim.fn.getcwd())
+            end,
             desc = "Run all tests in project",
           },
-          [prefix .. "<CR>"] = { function() require("neotest").summary.toggle() end, desc = "Test Summary" },
-          [prefix .. "o"] = { function() require("neotest").output.open() end, desc = "Output hover" },
-          [prefix .. "O"] = { function() require("neotest").output_panel.toggle() end, desc = "Output window" },
           [prefix .. "c"] = { function() require("neotest").output_panel.clear() end, desc = "Clear output" },
           ["]t"] = { function() require("neotest").jump.next() end, desc = "Next test" },
           ["[t"] = { function() require("neotest").jump.prev() end, desc = "previous test" },
