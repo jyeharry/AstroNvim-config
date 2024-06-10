1aad7220bee49abe1215c3fdf680360bd3380719
Jye Harry <jye.harry@nine.com.au>
2024-06-11 09:50:11 +1000

    Remove daily template

--- lua/plugins/obsidian-nvim.lua
+++ lua/plugins/obsidian-nvim.lua
@@ -120,7 +120,7 @@ return {
       -- Optional, if you want to change the date format of the default alias of daily notes.
       alias_format = "%B %-d, %Y",
       -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-      template = 'daily.md',
+      -- template = 'daily.md',
     },
 
     note_frontmatter_func = function(note)
